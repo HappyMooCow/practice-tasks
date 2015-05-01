eb293ca771024295b2d9343d00946a878f52f354
Josh <De4thNite@gmail.com>
2015-05-01 20:28:20 +1000

    Revert "Step 2"
    This reverts commit 681a88eabc340edfab23062613826a8325604ed5.

--- examGrades.cpp
+++ examGrades.cpp
@@ -7,10 +7,8 @@ int main() {
     cout << "Please enter your score (0-100)" << endl;
     cin >> score;
 
-    if (score >= 90) {
-    cout << "You got an A!" << endl;
-        if (score == 100) cout << "You got a perfect score!" << endl ;
+    if (score == 100) {
+        cout << "You got a perfect score!";
     }
-
 
 }
